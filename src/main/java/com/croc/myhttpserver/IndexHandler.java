7ceef01195542bb970d10ccf9208c72df3649ed0
MShestukhin <shist95@mail.ru>
2017-10-09 19:50:46 +0800

    add mime function

--- src/main/java/com/croc/myhttpserver/IndexHandler.java
+++ src/main/java/com/croc/myhttpserver/IndexHandler.java
@@ -1,12 +1,20 @@
 package com.croc.myhttpserver;
 
+import com.sun.activation.registries.MimeTypeFile;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import javax.activation.MimeType;
+import javax.activation.MimetypesFileTypeMap;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.nio.Buffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.nio.file.Files;
 
 /**
@@ -14,12 +22,52 @@ import java.nio.file.Files;
  */
 public class IndexHandler implements HttpHandler {
 
+    private String chooseMime(String path){
+        switch (path.substring(path.indexOf(".")+1))
+        {
+            case "htm":
+            case "html":
+            case "log":
+            case "cmd":
+            case "css":
+            case "csv":
+            case "javascript (Obsolete):":
+            case "plain:":
+            case "php:":
+            case "xml:":
+            case "markdown:":
+            case "txt":
+                return "text/"+path;
+            case "gif:":
+            case "jpeg:":
+            case "pjpeg:":
+            case "svg+xml":
+            case "tiff":
+            case "vnd.microsoft.icon":
+            case "vnd.wap.wbmp":
+            case "webp":
+            case  "png":
+                return "image/"+path;
+            case "pdf":
+            case "zip":
+            case "xml":
+            case "json:":
+                return "application/"+path;
+
+            default:
+                return "application/octet-stream";
+        }
+    }
     private void readChatFile(String path, HttpExchange exchange) throws IOException {
         File file=new File(getClass().getClassLoader().getResource(path).getFile());
         FileInputStream fileReader = new FileInputStream(file);
+        Headers header=exchange.getResponseHeaders();
+        header.add("Component-type", this.chooseMime(path));
+        System.out.print(path.substring(path.indexOf(".")+1));
         byte[] buff = new byte[(int) file.length()];
         OutputStream os=null;
         int i = 0;
+        int j=0;
         try {
             while ((i = fileReader.read(buff)) != -1) {
             }
