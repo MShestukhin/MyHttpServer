a9b0101359af064bdb3a64a365abf9e946b70d28
MShestukhin <shist95@mail.ru>
2017-10-10 15:04:14 +0800

    #12 add some log

--- src/main/java/com/croc/myhttpserver/Main.java
+++ src/main/java/com/croc/myhttpserver/Main.java
@@ -10,10 +10,10 @@ import java.net.InetSocketAddress;
  */
 public class Main {
     public static void main(String[] args) throws Exception{
-        System.out.print("Programm run \n");
         HttpServer server= HttpServer.create();
         server.bind(new InetSocketAddress(3333),0);
-        System.out.print("Server start on address"+server.getAddress());
+        System.out.print("**Listen on "+server.getAddress()+"\n");
+        System.out.print("**You can use http://127.0.0.1:3333 as httpserver");
         HttpContext context=server.createContext("/", new IndexHandler());
         server.start();
     }
